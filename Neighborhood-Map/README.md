4112fed800102962def9a37909d4c51e9c94b19d
wz30 <wz30@iu.edu>
2017-08-05 19:38:38 -0400

    Add files via upload

---
+++ Neighborhood-Map/README.md
@@ -0,0 +1,27 @@
+<strong>Udacity Neighborhood Map: Project 4</strong><br/>
+Author: Wei Zhang
+
+
+Basic Description:
+
+This project is SAP using Google Maps API to display the Washington DC area.<br/>
+The markers display many of the monuments and main attractions of the Washington DC area. <br/>
+<br/>
+Google Map Api
+
+Knockout 
+
+<strong>Functionality of the page includes:</strong> <br/> 
+	An interactive navigation bar to filter map and navigation bar results using knockout.js, <br/>
+	Display of the current Washington DC weather forecast using Weather Underground API, <br/>
+	Display of Flickr images within the Washington DC National Mall, <br/>
+		-all images are displayed in a modal upon a click handler with scroll functionality<br/>
+	A reset map button, <br/>
+	Responsive design giving the SAP the ability to be used and viewed on all web modern browsing devices
+
+<strong>Sources:</strong><br/>
+http://knockoutjs.com/documentation/introduction.html<br/>
+http://api.jquery.com/<br/>
+http://www.w3schools.com/
+
+
---
+++ Neighborhood-Map/js/script.js
@@ -0,0 +1,416 @@
+var map;
+var markersArray = [];
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp' +
+      '&signed_in=false&callback=initialize';
+  document.body.appendChild(script);
+}
+window.onload = loadScript;
+
+//Initialize the map and its contents
+function initialize() {  
+    var mapOptions = {
+        zoom: 14,
+        center: new google.maps.LatLng(38.896952, -77.029713),
+        mapTypeControl: false,
+        disableDefaultUI: true
+    };
+    if($(window).width() <= 1080) {
+        mapOptions.zoom = 13;
+    }
+    if ($(window).width() < 850 || $(window).height() < 595) {
+        hideNav();
+    }
+
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);  
+
+    setMarkers(markers);
+
+    setAllMap();
+
+    //Reset map on click handler and
+    //when window resize conditionals are met
+    function resetMap() {
+        var windowWidth = $(window).width();
+        if(windowWidth <= 1080) {
+            map.setZoom(13);
+            map.setCenter(mapOptions.center);
+        } else if(windowWidth > 1080) {
+            map.setZoom(14);
+            map.setCenter(mapOptions.center);   
+        }
+    }
+    $("#reset").click(function() {
+        resetMap();
+    });
+   $(window).resize(function() {
+        resetMap();
+    }); 
+}
+
+//Determines if markers should be visible
+//This function is passed in the knockout viewModel function
+function setAllMap() {
+  for (var i = 0; i < markers.length; i++) {
+    if(markers[i].boolTest === true) {
+    markers[i].holdMarker.setMap(map);
+    } else {
+    markers[i].holdMarker.setMap(null);
+    }
+  }
+}
+
+//Information about the different locations
+//Provides information for the markers
+var markers = [
+    {   
+    title: "The Thomas Jefferson Memorial",
+    lat: 38.881004, 
+    lng: -77.036463,
+    streetAddress: "701 East Basin SW",
+    cityAddress: "Washington, DC 20242",
+    url: "www.nps.gov/thje/index.htm",
+    id: "nav0",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The Lincoln Memorial",
+    lat: 38.889269, 
+    lng: -77.050176,
+    streetAddress: "2 Lincoln Memorial Cir",
+    cityAddress: "Washington, DC 20037",
+    url: "www.nps.gov/linc/index.htm",
+    id: "nav1",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The Washington Monument",
+    lat: 38.889484, 
+    lng: -77.0363733,
+    streetAddress: "2 15th St NW",
+    cityAddress: "Washington, DC 20007",
+    url: "www.nps.gov/wamo/index.htm",
+    id: "nav2",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The United States Capital",
+    lat: 38.889939, 
+    lng: -77.00905,
+    streetAddress: "East Capitol St NE & First St SE",
+    cityAddress: "Washington, DC 20004",
+    url: "www.visitthecapitol.gov",
+    id: "nav3",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "The White House",
+    lat: 38.8989013, 
+    lng: -77.0324048,
+    streetAddress: "1600 Pennsylvania Ave NW SW",
+    cityAddress: "Washington, DC 20500",
+    url: "www.whitehouse.gov",
+    id: "nav4",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {   
+    title: "The National WWII Memorial",
+    lat: 38.889443, 
+    lng: -77.040556,
+    streetAddress: "1750 Independence Ave SW",
+    cityAddress: "Washington, DC 20006",
+    url: "www.wwiimemorial.com/",
+    id: "nav5",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "National Gallery of Art",
+    lat: 38.890575, 
+    lng: -77.019764,
+    streetAddress: "6th & Constitution Ave NW",
+    cityAddress: "Washington, DC 20565",
+    url: "www.nga.gov/content/ngaweb.html",
+    id: "nav6",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "Smithsonian National Museum of Natural History",
+    lat: 38.890505, 
+    lng: -77.026031,
+    streetAddress: "10th St. & Constitution Ave. NW",
+    cityAddress: "Washington, DC 20560",
+    url: "www.mnh.si.edu",
+    id: "nav7",
+    visible: ko.observable(true),
+    boolTest: true
+    },
+    {
+    title: "Arts and Industries Building",
+    lat: 38.888771, 
+    lng: -77.024374,
+    streetAddress: "900 Jefferson Dr SW",
+    cityAddress: "Washington, DC 20560",
+    url: "www.si.edu/Museums/arts-and-industries-building",
+    id: "nav8",
+    visible: ko.observable(true),
+    boolTest: true
+    }   
+];
+
+//Get Google Street View Image for each inidividual marker
+    //Passed lat and lng to get each image location
+    //Had to pass title for whitehouse & different lat and lng to get images
+    //for White House and Capitol
+var headingImageView = [5, 235, 55, 170, 190, 240, -10, 10, 190];     
+var streetViewImage;
+var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=180x90&location=';
+
+function determineImage() {
+    if (i === 3) {
+        streetViewImage = streetViewUrl + '38.892052,-77.008888&fov=75&heading=' + headingImageView[i] + '&pitch=10';                 
+    } else if (i === 4) {
+        streetViewImage = streetViewUrl +
+                        markers[i].streetAddress + ',' + markers[i].cityAddress +
+                        '&fov=75&heading=' + headingImageView[i] + '&pitch=10';
+    } else {
+       streetViewImage = streetViewUrl +
+                        markers[i].lat + ',' + markers[i].lng +
+                        '&fov=75&heading=' + headingImageView[i] + '&pitch=10'; 
+                    }                   
+}
+
+//Sets the markers on the map within the initialize function
+    //Sets the infoWindows to each individual marker
+    //The markers are inidividually set using a for loop
+function setMarkers(location) {
+    
+    for(i=0; i<location.length; i++) {
+        location[i].holdMarker = new google.maps.Marker({
+          position: new google.maps.LatLng(location[i].lat, location[i].lng),
+          map: map,
+          title: location[i].title,
+          icon: {
+            url: 'img/marker.png',
+            size: new google.maps.Size(25, 40),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(12.5, 40)
+            },
+          shape: {
+            coords: [1,25,-40,-25,1],
+            type: 'poly'
+          }  
+        });
+
+        //function to place google street view images within info windows
+        determineImage();
+
+        //Binds infoWindow content to each marker
+        location[i].contentString = '<img src="' + streetViewImage + 
+                                    '" alt="Street View Image of ' + location[i].title + '"><br><hr style="margin-bottom: 5px"><strong>' + 
+                                    location[i].title + '</strong><br><p>' + 
+                                    location[i].streetAddress + '<br>' + 
+                                    location[i].cityAddress + '<br></p><a class="web-links" href="http://' + location[i].url + 
+                                    '" target="_blank">' + location[i].url + '</a>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: markers[i].contentString
+        });
+
+        //Click marker to view infoWindow
+            //zoom in and center location on click
+        new google.maps.event.addListener(location[i].holdMarker, 'click', (function(marker, i) {
+          return function() {
+            infowindow.setContent(location[i].contentString);
+            infowindow.open(map,this);
+            var windowWidth = $(window).width();
+            if(windowWidth <= 1080) {
+                map.setZoom(14);
+            } else if(windowWidth > 1080) {
+                map.setZoom(16);  
+            }
+            map.setCenter(marker.getPosition());
+            location[i].picBoolTest = true;
+          }; 
+        })(location[i].holdMarker, i));
+        
+        //Click nav element to view infoWindow
+            //zoom in and center location on click
+        var searchNav = $('#nav' + i);
+        searchNav.click((function(marker, i) {
+          return function() {
+            infowindow.setContent(location[i].contentString);
+            infowindow.open(map,marker);
+            map.setZoom(16);
+            map.setCenter(marker.getPosition());
+            location[i].picBoolTest = true;
+          }; 
+        })(location[i].holdMarker, i));
+    }
+}
+
+//Query through the different locations from nav bar with knockout.js
+    //only display markers and nav elements that match query result
+var viewModel = {
+    query: ko.observable(''),
+};
+
+viewModel.markers = ko.dependentObservable(function() {
+    var self = this;
+    var search = self.query().toLowerCase();
+    return ko.utils.arrayFilter(markers, function(marker) {
+    if (marker.title.toLowerCase().indexOf(search) >= 0) {
+            marker.boolTest = true;
+            return marker.visible(true);
+        } else {
+            marker.boolTest = false;
+            setAllMap();
+            return marker.visible(false);
+        }
+    });       
+}, viewModel);
+
+ko.applyBindings(viewModel);
+
+//show $ hide markers in sync with nav
+$("#input").keyup(function() {
+setAllMap();
+});
+
+//Hide and Show entire Nav/Search Bar on click
+    // Hide/Show Bound to the arrow button
+    //Nav is repsonsive to smaller screen sizes
+var isNavVisible = true;
+function noNav() {
+    $("#search-nav").animate({
+                height: 0, 
+            }, 500);
+            setTimeout(function() {
+                $("#search-nav").hide();
+            }, 500);    
+            $("#arrow").attr("src", "img/down-arrow.gif");
+            isNavVisible = false;
+}
+function yesNav() {
+    $("#search-nav").show();
+            var scrollerHeight = $("#scroller").height() + 55;
+            if($(window).height() < 600) {
+                $("#search-nav").animate({
+                    height: scrollerHeight - 100,
+                }, 500, function() {
+                    $(this).css('height','auto').css("max-height", 439);
+                });  
+            } else {
+            $("#search-nav").animate({
+                height: scrollerHeight,
+            }, 500, function() {
+                $(this).css('height','auto').css("max-height", 549);
+            });
+            }
+            $("#arrow").attr("src", "img/up-arrow.gif");
+            isNavVisible = true;
+}
+
+function hideNav() {
+    if(isNavVisible === true) {
+            noNav();
+            
+    } else {
+            yesNav();  
+    }
+}
+$("#arrow").click(hideNav);
+
+//Hide Nav if screen width is resized to < 850 or height < 595
+//Show Nav if screen is resized to >= 850 or height is >= 595
+    //Function is run when window is resized
+$(window).resize(function() {
+    var windowWidth = $(window).width();
+    if ($(window).width() < 850 && isNavVisible === true) {
+            noNav();
+        } else if($(window).height() < 595 && isNavVisible === true) {
+            noNav();
+        }
+    if ($(window).width() >= 850 && isNavVisible === false) {
+            if($(window).height() > 595) {
+                yesNav();
+            }
+        } else if($(window).height() >= 595 && isNavVisible === false) {
+            if($(window).width() > 850) {
+                yesNav();
+            }     
+        }    
+});
+
+//Expand .forecast div on click to see Weather Underground forecast
+//and shrink back when additionally clicked
+    //size is repsonsive to smaller screens
+var weatherContainer = $("#weather-image-container");
+var isWeatherVisible = false;
+weatherContainer.click(function() {
+    if(isWeatherVisible === false) {
+        if($(window).width() < 670) {
+            $(".forecast li").css("display", "block");
+            weatherContainer.animate({
+                width: "245"
+            }, 500);
+        } else {
+            $(".forecast li").css("display", "inline-block");
+            weatherContainer.animate({
+                width: "380"
+            }, 500);
+        }
+        isWeatherVisible = true;
+    } else {
+        weatherContainer.animate({
+        width: "80"
+    }, 500);
+        isWeatherVisible = false;
+    }
+});
+
+//GET Weather Underground JSON
+    //Append Weather forecast for Washington DC to .forecast
+    //If error on GET JSON, display message
+var weatherUgUrl = "http://api.wunderground.com/api/8b2bf4a9a6f86794/conditions/q/DC/Washington.json";
+
+$.getJSON(weatherUgUrl, function(data) {
+    var list = $(".forecast ul");
+    detail = data.current_observation;
+    list.append('<li>Temp: ' + detail.temp_f + '° F</li>');
+    list.append('<li><img style="width: 25px" src="' + detail.icon_url + '">  ' + detail.icon + '</li>');
+}).error(function(e){
+        $(".forecast").append('<p style="text-align: center;">Sorry! Weather Underground</p><p style="text-align: center;">Could Not Be Loaded</p>');
+    });
+
+//Hide and show Weather forecast div from screen on click
+var isWeatherImageVisible = true;
+var hideWeatherArrow = $("#hide-weather").find("img");
+function hideWeather() {
+    if(isWeatherImageVisible === true) {
+            $("#weather-image-container").animate({
+                height: 0,
+                paddingTop: 0
+            }, 300);
+        isWeatherImageVisible = false;
+        hideWeatherArrow.attr("src", "img/small-down-arrow.png"); 
+    } else {
+            $("#weather-image-container").animate({
+                height: 60,
+                paddingTop: 5
+            }, 300);
+        isWeatherImageVisible = true;
+        hideWeatherArrow.attr("src", "img/small-up-arrow.png");
+    }
+}
+
+$("#hide-weather").click(hideWeather);
