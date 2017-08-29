e78dd5bfe5480f952f329b9e6938895c8fa38f2a
wz30 <wz30@iu.edu>
2017-08-29 12:06:19 -0400

    new one

--- Neighborhood-Map/README.md
+++ Neighborhood-Map/README.md
@@ -1,27 +1,23 @@
-<strong>Udacity Neighborhood Map: Project 4</strong><br/>
-Author: Wei Zhang
+# neighborhood-map-udacity
+Udacity project with knockout JS framework and google maps API.
+
+## Setup:
+* Clone or download the repo.
+  * Extract files.
+  * Open index.html
 
 
-Basic Description:
+## Expected Functionality:
+* Google map loaded from Maps API.
+* Map markers located at Points of Interest (Restaurants)
+* Points of interest data loaded via Foursquare API (image, links, geo location data).
+* API errors are handled with warning message at top of page
+* Points of Interest markers all present upon page load
+* Points of Interest markers can be filtered via a Dropdown filter menu (Neighborhoods)
+* Page is responsive.  Navbar is hidden and toggleable with hamburger when viewing on mobile (screens smaller than 768px width).
+* Page is built using Knockout JS
 
-This project is SAP using Google Maps API to display the Washington DC area.<br/>
-The markers display many of the monuments and main attractions of the Washington DC area. <br/>
-<br/>
-Google Map Api
-
-Knockout 
-
-<strong>Functionality of the page includes:</strong> <br/> 
-	An interactive navigation bar to filter map and navigation bar results using knockout.js, <br/>
-	Display of the current Washington DC weather forecast using Weather Underground API, <br/>
-	Display of Flickr images within the Washington DC National Mall, <br/>
-		-all images are displayed in a modal upon a click handler with scroll functionality<br/>
-	A reset map button, <br/>
-	Responsive design giving the SAP the ability to be used and viewed on all web modern browsing devices
-
-<strong>Sources:</strong><br/>
-http://knockoutjs.com/documentation/introduction.html<br/>
-http://api.jquery.com/<br/>
-http://www.w3schools.com/
-
-
+## Attributions:
+* [Google Maps API](https://developers.google.com/maps/)
+* [Foursquare API](https://developer.foursquare.com/)
+* [Bootsnipp](http://bootsnipp.com/snippets/featured/admin-side-menu) for responsive navbar.
---
+++ Neighborhood-Map/js/starting-data.js
@@ -0,0 +1,70 @@
+
+// My list of baseline restaurant objects.  "ID" is the key for the Foursquare
+// API call.  Name is manually entered here for convenience.
+// Neighborhood data is manually entered; had a hard time finding a reasonable
+// API to automatically generate neighboor data by location.
+
+var restObjArray = [
+    {
+        'name': 'Darwin Cafe',
+        'id': '4c4e30871b8e1b8dd9d3c426',
+        'notes': 'One of our favorite spots, by proxy of being located about 10 feet from our building.  The charcuterie, salads, and sandwiches are delicious and satisfying.',
+        'neighborhood': 'SOMA'
+    },
+
+    {
+        'name': 'Yummy Yummy',
+        'id': '443fde11f964a5203a321fe3',
+        'notes': 'Possibly the best pho in the city. Certainly a contested title, but the prices, portions, flavor, and options are amazing.  And holy crap, do I love pho.  A good run to brunch spot!',
+        'neighborhood': 'Inner Sunset'
+    },
+
+    {
+        'name': 'Brickhouse Cafe',
+        'id': '440db456f964a52097301fe3',
+        'notes': 'A surprisingly underestimated bar / cafe with delicious burgers, pot pies, and kickass brunch.  Pancake batter covered bacon?  Absolutely.',
+        'neighborhood': 'SOMA'
+    },
+
+    {
+        'name': 'Neighbor Bakehouse',
+        'id': '5272781811d2ce55eb5981df',
+        'notes': 'Another obscured gem in the Dogpatch district, Neighbor Bakehouse has some of the best pull-apart bread to be found in the bay area.  So far, every pastry we\'ve purchased has been incredibly satisfying.  It\'s always a struggle not to purchase something when we run past.',
+        'neighborhood': 'Dogpatch'
+    },
+
+    {
+        'name': 'Loló',
+        'id': '52eb109611d2f169d2d80599',
+        'notes': 'We wandered into this place by chance, and fell in love with the ceviche.  Every dish so far has been excellent - tacos, avocado toast, egg scrambles - but the ceviche really stands out.',
+        'neighborhood': 'Mission District'
+    },
+
+    {
+        'name': 'Co Nam',
+        'id': '505a8cfde4b08096cab53875',
+        'notes': 'Randomly chose this place after a long run, and discovered a fantastic Vietnamese fusion place.  The chicken liver skewers were delicious, and the pho was phenomenal.  Of course, for $14, it damn well better be.',
+        'neighborhood': 'Nob Hill'
+    },
+
+    {
+        'name': 'Smokestack',
+        'id': '541e02fd498e5af2958c8b1b',
+        'notes': 'We first tried the "meat stick" at a food fair in Dogpatch, and immediately resolved to actually visit the location.  The brisket melts in your mouth.  It is glorious.  All else is secondary.',
+        'neighborhood': 'Dogpatch'
+    },
+
+    {
+        'name': 'Park Chow',
+        'id': '3fd66200f964a52026f11ee3',
+        'notes': '"Comfort food" sums this up nicely.  Classic brunch fare, done right for the big city.',
+        'neighborhood': 'Inner Sunset'
+    },
+
+    {
+        'name': 'MarketBar',
+        'id': '49ef6148f964a520ab681fe3',
+        'notes': 'A reasonably solid choice in terms of food options and quality, but the price reflects the surroundings.  Still, eating on the terrace under the sun, watching the tourists wander by - it\'s an enjoyable experience.',
+        'neighborhood': 'Embarcadero'
+    }
+];
